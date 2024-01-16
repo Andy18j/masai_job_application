7fb24313d8937cb5289e0b177c5ff93bdb376a1c
Andy18j <115467259+Andy18j@users.noreply.github.com>
2024-01-16 17:37:02 +0530

    ad

--- model/job.model.js
+++ model/job.model.js
@@ -10,7 +10,7 @@ const jobSchema = mongoose.Schema({
     role     : {type:String,enum:["Frontend", "Backend", "FullStack", "Other"],required:true},
     level:    { type: String,enum: ["Junior", "Senior", "Other"],required: true},
     contract: {type: String,enum: ["Full Time", "Part Time", "Other"],required: true},
-    position: {type: String, required: true},
+    position: {type: String,enum:["Backend Developer","Junior Frontend Developer","Junior Backend Developer","FSD"], required: true},
     language: {type: String,enum: ["Javascript", "Java", "C", "C++", "Other"],required: true
     }
 })
